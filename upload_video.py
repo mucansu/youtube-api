249086cb38cfd2fb2f206b1ca4edb0fe2ae7ba43
Mustafa <mucansu@gmail.com>
2024-04-23 14:59:04 +0000

    small changes to get obj via id from external service

--- upload_video.py
+++ upload_video.py
@@ -66,7 +66,7 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
 
-def get_authenticated_service(args):
+def get_authenticated_service():
   flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
     scope=YOUTUBE_UPLOAD_SCOPE,
     message=MISSING_CLIENT_SECRETS_MESSAGE)
@@ -80,20 +80,20 @@ def get_authenticated_service(args):
   return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
     http=credentials.authorize(httplib2.Http()))
 
-def initialize_upload(youtube, options):
+def initialize_upload(youtube,file, body):
   tags = None
-  if options.keywords:
-    tags = options.keywords.split(",")
+  if body.keywords:
+    tags = body.keywords.split(",")
 
   body=dict(
     snippet=dict(
-      title=options.title,
-      description=options.description,
+      title=body.title,
+      description=body.description,
       tags=tags,
-      categoryId=options.category
+      categoryId=body.category
     ),
     status=dict(
-      privacyStatus=options.privacyStatus
+      privacyStatus=body.privacyStatus
     )
   )
 
@@ -112,7 +112,7 @@ def initialize_upload(youtube, options):
     # practice, but if you're using Python older than 2.6 or if you're
     # running on App Engine, you should set the chunksize to something like
     # 1024 * 1024 (1 megabyte).
-    media_body=MediaFileUpload(options.file, chunksize=-1, resumable=True)
+    media_body=MediaFileUpload(file, chunksize=-1, resumable=True)
   )
 
   resumable_upload(insert_request)
@@ -153,6 +153,11 @@ def resumable_upload(insert_request):
       time.sleep(sleep_seconds)
 
 if __name__ == '__main__':
+  #body diğer servisten obje olarak gelecek
+  #bu objenin attribute lerine (body.file, body.description, body.keywords vs.) ulasabiliriz 
+  url='https://ksv.mintyazilim.com/api/course/program/?format=json&id=' + id
+  response=requests.get(url)
+  body = response.json()
   argparser.add_argument("--file", required=True, help="Video file to upload")
   argparser.add_argument("--title", help="Video title", default="Test Title")
   argparser.add_argument("--description", help="Video description",
@@ -166,11 +171,11 @@ if __name__ == '__main__':
     default=VALID_PRIVACY_STATUSES[0], help="Video privacy status.")
   args = argparser.parse_args()
 
-  if not os.path.exists(args.file):
-    exit("Please specify a valid file using the --file= parameter.")
+  if not os.path.exists(body.file):
+    exit("Istenen video bulunamadı")
 
   youtube = get_authenticated_service(args)
   try:
-    initialize_upload(youtube, args)
+    initialize_upload(youtube, body)
   except HttpError as e:
     print ("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
