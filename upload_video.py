c7ea86d2020c7a2748f0b39c3525bcae590917e9
Mustafa <mucansu@gmail.com>
2024-05-12 18:33:40 +0000

    code beuatified

--- upload_video.py
+++ upload_video.py
@@ -16,12 +16,6 @@ import schedule as sch
 import requests
 from oauth2client.client import HttpAccessTokenRefreshError
 
-
-"""
-path = '/home/mint/mustafa2/youtubeapi/video'
-for filename in os.listdir(path):
-    print(filename)
-"""
 path = '/home/mint/mustafa2/youtubeapi/videolar'
 uploaded_path='/home/mint/mustafa2/youtubeapi/yüklenen_videolar'
 def job():
@@ -42,9 +36,10 @@ def job():
         "snippet": {
             "title": data["title"],
             "description": data["description"], 
-            "keywords" : data["tag"],
+            "keywords" : id,
             "file": os.path.join(path, filename), 
             "category": data["categoryId"],
+            
              
         },
         "status": {
@@ -53,19 +48,16 @@ def job():
     }
     upload_successful = False
     try:
-        youtube = get_authenticated_service()
-        print ("Video yükleniyor...")
+        youtube = get_authenticated_service()      
         initialize_upload(youtube, body)
         upload_successful = True
     except HttpError as e:
         print ("An HTTP error %d occurred:\n%s" % (e.resp.status, e.content))
         
-    if upload_successful:
-      print("Video Yüklendi")
+    if upload_successful:      
       move_files(filename)
   else:
-      print(f"Error: {response.status_code}")
-      print("Yükleme işlemi başarısız oldu")
+      print(f"Error: {response.status_code}")      
 def schedule():
     sch.every(70).minutes.do(job)
 
@@ -75,7 +67,7 @@ def schedule():
 
 
 def check_files():
-    filename = id = None # bu kısım gerekli, çünkü filename yok ise program çalışmayı durduruyor
+    filename = id = None
     for filename in os.listdir(path):
         id = filename.split('.')[0]
         print(id)
@@ -84,54 +76,19 @@ def check_files():
 
 def move_files(filename):
     os.rename(os.path.join(path, filename), os.path.join(uploaded_path, filename))
-    print("Dosya taşındı")
-
-"""
-def get_video_info(file, title="Test Title", description="Test Description", category="22", keywords="egitim,bilgi", privacyStatus='public'):
-    video_info = {
-        "file": file,
-        "title": title,
-        "description": description,
-        "category": category,
-        "keywords": keywords,
-        "privacyStatus": privacyStatus
-    }
-    return video_info"""
-
-# Tekrar deneme işini kendimiz uygulamada yaptığımız için bu pakete kafasına göre ikinci kez deneme yapmamasını söylüyoruz
-# Biz deneme mantığını kendimiz yapacağız.
 httplib2.RETRIES = 1
-
 # Maximum number of times to retry before giving up.
 MAX_RETRIES = 10
-
 # Always retry when these exceptions are raised.
 RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError)
 
 # Always retry when an apiclient.errors.HttpError with one of these status
 # codes is raised.
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
-
-# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
-# the OAuth 2.0 information for this application, including its client_id and
-# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
-# the Google API Console at
-# https://console.cloud.google.com/.
-# Please ensure that you have enabled the YouTube Data API for your project.
-# For more information about using OAuth2 to access the YouTube Data API, see:
-#   https://developers.google.com/youtube/v3/guides/authentication
-# For more information about the client_secrets.json file format, see:
-#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 CLIENT_SECRETS_FILE = "client_secrets.json"
-
-# This OAuth 2.0 access scope allows an application to upload files to the
-# authenticated user's YouTube channel, but doesn't allow other types of access.
 YOUTUBE_UPLOAD_SCOPE = "https://www.googleapis.com/auth/youtube.upload"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
-
-# This variable defines a message to display if the CLIENT_SECRETS_FILE is
-# missing.
 MISSING_CLIENT_SECRETS_MESSAGE = """
 WARNING: Please configure OAuth 2.0
 
@@ -150,12 +107,10 @@ https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
 
 VALID_PRIVACY_STATUSES = ("public", "private", "unlisted")
 
-
 def get_authenticated_service():
     flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE,
     scope=YOUTUBE_UPLOAD_SCOPE,
     message=MISSING_CLIENT_SECRETS_MESSAGE)
-
     storage = Storage("%s-oauth2.json" % sys.argv[0])
     credentials = storage.get()
 
@@ -165,14 +120,10 @@ def get_authenticated_service():
         return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                      http=credentials.authorize(httplib2.Http()))
     except HttpAccessTokenRefreshError:
-        # token expired, aynı giriş bilgileri ile refresh ediyoruz.
+        # token expired
         http = credentials.authorize(httplib2.Http())
-        credentials.refresh(http)
-
-        # yenilenmiş token ile yeniden işleme devam ediyoruz
-        storage.put(credentials)
-
-        # servisi tekrar çalıştırıyoruz
+        credentials.refresh(http)        
+        storage.put(credentials)        
         return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
                      http=credentials.authorize(httplib2.Http()))
 
@@ -180,10 +131,6 @@ def initialize_upload(youtube, body):
   tags = None
   if body['snippet']['keywords']:
     tags = body['snippet']['keywords'].split(",")
-
-# Bu kısımda video nesnesini oluşturuyoruz, ve bilgilerini de (başlık, acıklama, etiketler vs.) ekliyoruz.
-# bu bilgileri client tarafından almış olmamız gerekiyor, client tarafında bu bilgilerin nasıl işlendiğini
-# görmem lazım. 
   upload_body=dict( 
         snippet=dict(
             title=body['snippet']['title'],
@@ -199,21 +146,9 @@ def initialize_upload(youtube, body):
   # Call the API's videos.insert method to create and upload the video.
   insert_request = youtube.videos().insert(
     part=",".join(upload_body.keys()),
-    body=upload_body,
-    # The chunksize parameter specifies the size of each chunk of data, in
-    # bytes, that will be uploaded at a time. Set a higher value for
-    # reliable connections as fewer chunks lead to faster uploads. Set a lower
-    # value for better recovery on less reliable connections.
-    #
-    # Setting "chunksize" equal to -1 in the code below means that the entire
-    # file will be uploaded in a single HTTP request. (If the upload fails,
-    # it will still be retried where it left off.) This is usually a best
-    # practice, but if you're using Python older than 2.6 or if you're
-    # running on App Engine, you should set the chunksize to something like
-    # 1024 * 1024 (1 megabyte).
+    body=upload_body,    
     media_body=MediaFileUpload(body['snippet']['file'], chunksize=-1, resumable=True)
   )
-
   resumable_upload(insert_request)
 
 # This method implements an exponential backoff strategy to resume a
@@ -252,24 +187,6 @@ def resumable_upload(insert_request):
       time.sleep(sleep_seconds)
 if __name__ == '__main__':
   path = '/home/mint/mustafa2/youtubeapi/videolar'
-  uploaded_path='/home/mint/mustafa2/youtubeapi/yüklenen_videolar'
-  
+  uploaded_path='/home/mint/mustafa2/youtubeapi/yüklenen_videolar'  
   print("something happening")
   schedule()
-"""
-  if not os.path.exists(body['snippet']['file']):
-    exit("Please specify a valid file using the --file= parameter.")
-
-  body = {
-        "snippet": {
-            "file": path, 
-            "title": "Deneme başlık",  
-            "description": "Deneme açıklama",
-            "keywords": "egitim,ders",
-            "category": "22"
-        },
-        "status": {
-            "privacyStatus": "public"
-        }
-    }
-  """
